8a087b744d0317109d06dac2214e943a1936a9a2
yetem94 <yetem94@gmail.com>
2016-05-20 23:57:35 +0200

    poprawka w ignore

---
+++ core/src/Actors/people/Out/AbstractOutPerson.java
@@ -0,0 +1,45 @@
+package Actors.people.Out;
+
+import Actors.MyActor;
+import Actors.buttons.AbstractButton;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.Array;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * Created by Marcin on 2016-05-20.
+ */
+public class AbstractOutPerson extends MyActor {
+    protected Array<Image> images;
+    protected Random rand;
+
+    public AbstractOutPerson(Stage stage){
+        super(stage);
+    }
+
+    public void initOtherFields(){
+        rand = new Random();
+        images = new Array<Image>();
+    }
+
+    @Override
+    public void setPositionNotNormall(int x, int y) {
+
+    }
+
+    public void setMyOwnClickListener(ClickListener listener){
+        for(Image img: images){
+            img.addListener(listener);
+        }
+    }
+
+    public void setPosition(int x, int y){
+        for(Image img: images){
+            img.setPosition(x,y);
+        }
+    }
+}
