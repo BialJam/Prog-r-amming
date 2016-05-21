d042113a4ba1472b44357bd3705144918e574e2a
sury <asda@asda.pl>
2016-05-21 22:29:33 +0200

    vectors

--- core/src/Utils/Vectors.java
+++ core/src/Utils/Vectors.java
@@ -7,9 +7,9 @@ import com.badlogic.gdx.utils.Array;
 /**
  * Created by sury on 21.05.16.
  */
-public class Places {
+public class Vectors {
 
-    
+
     private static MathUtils math = new MathUtils();
 
     public static Vector2 getEat() {
@@ -21,5 +21,18 @@ public class Places {
     }
     public static Vector2 getquit(){
         return new Vector2(530 ,560);
+    }
+
+    public static Vector2 getRandom(){
+        return new Vector2(math.random(300,1000),math.random(100,650));
+    }
+
+    public static Vector2 goFight(){
+        return new Vector2(math.random(500,700),math.random(250,350));
+    }
+
+    public float vectorLength(Vector2 start, Vector2 end){
+        Vector2 newVewctor = new Vector2(end.x - start.x, end.y - start.y);
+        return  newVewctor.len();
     }
 }
