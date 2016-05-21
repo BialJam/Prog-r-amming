96879852e6ee1e1ca6d5d323fd7f2d591729b698
sury <asda@asda.pl>
2016-05-21 16:14:21 +0200

    Made all lights

--- core/src/screens/InSide.java
+++ core/src/screens/InSide.java
@@ -4,6 +4,8 @@ import Actors.ActorString;
 import Actors.Background;
 import Actors.people.In.BadassIn;
 import Utils.JustABodyWall;
+import Utils.JustLights;
+import box2dLight.Light;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
@@ -20,6 +22,7 @@ import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.badlogic.gdx.scenes.scene2d.actions.SequenceAction;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.utils.Array;
 import com.mygdx.game.MyGdxGame;
 import com.mygdx.game.Statics;
 
@@ -29,13 +32,18 @@ import com.mygdx.game.Statics;
 public class InSide extends MyScreen implements Screen {
     MyGdxGame root;
     BadassIn badass;
+
     int time;
+    float timeLights = 0;
+
     double deltatime;
     ActorString timerString;
     private BitmapFont font;
     Box2DDebugRenderer debugRenderer;
     Matrix4 debugMatrix;
     World world;
+    Array<JustLights> parket = new Array<JustLights>();
+    Array<JustLights> parket2 = new Array<JustLights>();
 
     public InSide(MyGdxGame root){
         super();
@@ -52,8 +60,9 @@ public class InSide extends MyScreen implements Screen {
         debugRenderer = new Box2DDebugRenderer();
         world = Statics.world;
         debugMatrix =  game.getBatch().getProjectionMatrix();
-        JustABodyWall wall = new JustABodyWall(100,100,200,200);
 
+        createDemFuckingWalls();
+        createDemLights();
     }
 
     @Override
@@ -74,12 +83,16 @@ public class InSide extends MyScreen implements Screen {
 
         game.draw();
         debugRenderer.render(world, debugMatrix);
+        Statics.rayHandler.setCombinedMatrix(game.getCamera().combined);
+        Statics.rayHandler.updateAndRender();
+
 
 
 
         if(Gdx.input.isKeyJustPressed(Input.Keys.C)) {
             badass.randomize_direct();
         }
+        SwitchLights();
 
     }
 
@@ -127,5 +140,61 @@ public class InSide extends MyScreen implements Screen {
         TextureAtlas atlas = Statics.assetManager.get("Other/Other.pack");
         Skin skin = new Skin(atlas);
         new Background(new Image(skin.getDrawable("bg_in")),game);
+    }
+    // kieryunek lewy dol -> prawy dol ->_prawa gora -> lewa gora
+    private void createDemFuckingWalls(){
+        new JustABodyWall(100,100,140,138,45);
+        new JustABodyWall(80,110,220,175,0);
+        new JustABodyWall(40,40,300,60,-45);
+        new JustABodyWall(280,25,610,30,0);
+        new JustABodyWall(110,60,988,72,45);
+        new JustABodyWall(180,60,1270,190,45);
+        new JustABodyWall(50,70,1363,422,45);
+        new JustABodyWall(10,140,1353,640,0);
+        new JustABodyWall(20,230,15,535,0);
+        new JustABodyWall(670,10,690,760,0);
+
+    }
+
+    private void createDemLights(){
+        new JustLights(1100,150,400,Color.RED);
+        new JustLights(1300,350,400,Color.RED);
+        new JustLights(80,730,800,Color.WHITE);
+        new JustLights(1250,730,800,Color.WHITE);
+        new JustLights(700,730,800,Color.WHITE);
+        new JustLights(50,300,250,Color.WHITE);
+
+//        Disco Ligths
+        parket.add(new JustLights(400,150,400,Color.GOLD));
+        parket.add(new JustLights(800,150,400,Color.BLUE));
+
+        parket2.add(new JustLights(600,70,300,Color.GREEN));
+        parket2.add(new JustLights(380,170,300,Color.GREEN));
+        parket2.add(new JustLights(820,170,300,Color.GREEN));
+
+        for(JustLights lgt : parket2){
+            lgt.light.setActive(false);
+        }
+    }
+
+    private void SwitchLights(){
+        timeLights += Gdx.graphics.getDeltaTime();
+        if(timeLights>1.0){
+            for(JustLights lgt : parket){
+                if(lgt.light.isActive()){
+                    lgt.light.setActive(false);
+                }else {
+                    lgt.light.setActive(true);
+                }
+            }
+            for(JustLights lgt : parket2){
+                if(lgt.light.isActive()){
+                    lgt.light.setActive(false);
+                }else {
+                    lgt.light.setActive(true);
+                }
+            }
+            timeLights =0;
+        }
     }
 }
