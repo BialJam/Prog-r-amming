f4bf3404a3e7d0ef1f243e6eddc8b8783ab8a459
yetem94 <yetem94@gmail.com>
2016-05-22 04:21:38 +0200

    sprzataczka lazi i sprzata, lekko zbugowana

--- core/src/Actors/Vomit.java
+++ core/src/Actors/Vomit.java
@@ -1,17 +1,35 @@
 package Actors;
 
+import Actors.people.In.CleanWoman;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.mygdx.game.MyGdxGame;
+import com.sun.rowset.internal.InsertRow;
+import screens.InSide;
 
 /**
  * Created by Marcin on 2016-05-22.
  */
 public class Vomit extends MyActor {
-
+    private Vomit myInstance;
 
     public Vomit(Stage stage) {
         super(stage);
+        myInstance = this;
+        image.addListener(new ClickListener(){
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                CleanWoman cw = InSide.getFreeCleanWoman();
+                if(cw!=null){
+                    cw.free = false;
+                    cw.target = myInstance;
+
+                }
+                return super.touchDown(event, x, y, pointer, button);
+            }
+        });
     }
 
     @Override
--- core/src/Actors/people/In/AbstractInPerson.java
+++ core/src/Actors/people/In/AbstractInPerson.java
@@ -51,13 +51,13 @@ public class AbstractInPerson extends MyActor {
     public PersonBody body;
 
     private MathUtils math = new MathUtils();
-    private int animate = 0;
+
     private float deltatime;
     private boolean tick = false;
-    private int rotate = 1;
+
 
     protected boolean moveRotate = true;
-    public float speedX = 0.5f, speedY = 0.5f, angle = 0.0f, prev_angle, maxSpeed;
+
 
     // DEBUG
     BitmapFont font;
@@ -188,16 +188,7 @@ public class AbstractInPerson extends MyActor {
 
     }
 
-    private void animate() {
-        animate++;
 
-        image.setOrigin(image.getWidth() / 2, image.getHeight() / 2);
-
-        if (animate == 13)
-            animate = -12;
-        if (animate % 3 == 0)
-            image.rotateBy(animate);
-    }
 
     public void move() {
         animate();
@@ -215,22 +206,7 @@ public class AbstractInPerson extends MyActor {
 
     }
 
-    public void setAngle(Vector2 way) {
-        double theta = Math.atan2(-way.y, way.x);
-        theta += Math.PI / 2.0;
-        prev_angle = angle;
-        angle = (float) Math.toDegrees(theta);
 
-        if (angle < 0) {
-            angle += 360;
-        }
-
-        rotate = (int) prev_angle - (int) angle;
-        if (rotate < 0) {
-            rotate += 360;
-        }
-        image.rotateBy(rotate);
-    }
 
     public void setPosition(int x, int y) {
         image.setPosition(x, y);
